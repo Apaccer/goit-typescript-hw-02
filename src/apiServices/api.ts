74b0fad12c00f3d15962fa60a165b07882b432ea
Apaccer <142975619+Apaccer@users.noreply.github.com>
2024-05-06 11:00:33 +0300

    hw-ts-02-am3

--- src/apiServices/api.ts
+++ src/apiServices/api.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
+
 const apiKey = "rYmK2FB17omhqrvA6G0Vcmg-dpqrRFElkMnY1DbtE9Y";
+
 export const getImagesByQuery = async (query: string, page: number) => {
   const { data } = await axios.get(
     `https://api.unsplash.com/search/photos/?client_id=${apiKey}&page=${page}&query=${query}&per_page=20`
   );
+
   return data;
 };
